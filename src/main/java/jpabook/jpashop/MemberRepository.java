d79f2acb36fb688b72091d6508205b5624935767
BaikSeunghan <tmdgks103@naver.com>
2022-04-07 22:50:50 +0900

    View 환경설정, JPA와 DB(H2 DB) 설정 및 동작확인

---
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,22 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext // 스프링부트가 해당 어노테이션이 붙은 객체를 찾아서 주입해준다
+    private EntityManager em;
+
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+}
